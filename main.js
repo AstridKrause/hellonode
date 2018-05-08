c6042610b5dfebccf9ad1c4f34313537acee9d7b
Astrid Krause <astrid.krause@dxc.com>
2018-05-08 08:31:31 +0000

    Fixed main.js

--- main.js
+++ main.js
@@ -3,13 +3,14 @@ var http = require('http');
 var fs = require('fs');
 
 // configur= e our HTTP server
-var server = http.createServer(function  (req, res) {
-  fs.readFile('index.html', function(err, data) {
+var server = http.createServer(function (request, response) {
+//  response.writeHead(200, {"Content-Type": "text/plain"});
+//  response.end("UNDERDOCKS DEVOPS EXAMPLE Application\nWELCOME "+process.env.MYENV);
+   fs.readFile('index.html', function(err, data) {
       res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
      res.end();
-  });
-
+});
 
 // listen on localhost:8000
 server.listen(8000);
