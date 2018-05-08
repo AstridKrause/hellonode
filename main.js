7a1adfc882e3ae728c087d555ffce96ecb133a01
Astrid Krause <astrid.krause@dxc.com>
2018-05-08 08:37:06 +0000

    added demofile

--- main.js
+++ main.js
@@ -1,17 +1,10 @@
-// load the http module
 var http = require('http');
 var fs = require('fs');
-
-// configur= e our HTTP server
-var server = http.createServer(function (request, response) {
-//  response.writeHead(200, {"Content-Type": "text/plain"});
-//  response.end("UNDERDOCKS DEVOPS EXAMPLE Application\nWELCOME "+process.env.MYENV);
-   fs.readFile('index.html', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/html'});
+http.createServer(function (req, res) {
+  //Open a file on the server and return it's content:
+  fs.readFile('demofile1.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
-     res.end();
-});
-
-// listen on localhost:8000
-server.listen(8000);
-console.log("Server listening at http://127.0.0.1:8000/");
+    return res.end();
+  });
+}).listen(8080);
