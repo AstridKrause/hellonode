163c4b12d91b28b6b3b67587dbc61b47f6e59186
Astrid Krause <astrid.krause@dxc.com>
2018-05-08 15:20:29 +0000

    environment variables fully functioning

--- main.js
+++ main.js
@@ -2,21 +2,33 @@
 var http = require('http');
 var fs = require('fs');
 
-// xconfigure our HTTP server
 
-http.createServer(function  (req, res) {
-  fs.readFile('demofile2.html', function(err, data) {
-      console.log('Step 1');
+// xconfigure our HTTP server
+var server = http.createServer(function (request, response) {
+   response.writeHead(200, {'Content-Type': 'text/html'});
+
+  fs.readFile('demofile3.html', function(err, data3) {
       if (err) {
          return console.log(err);
       }
-      console.log('Step 2');
-      res.writeHead(200, '{'Content-Type': 'text/html'});
-      console.log('Step 3);
+     response.write(data3);
+     	response.write('<tr><td>APP_ENV</td><td>' + process.env.APP_ENV + '</td></tr>');
+	response.write('<tr><td>LOGLEVEL</td><td>' + process.env.LOGLEVEL + '</td></tr>');
+     	response.write('<tr><td>SMTP_SERVER</td><td>' + process.env.SMTP_SERVER + '</td></tr>');
+	response.write('<tr><td>DATABASE_NAME</td><td>' + process.env.DATABASE_NAME + '</td></tr>');
+     	response.write('<tr><td>DATABASE_TYPE</td><td>' + process.env.DATABASE_TYPE + '</td></tr>');
+	response.write('<tr><td>DATABASE_URL</td><td>' + process.env.DATABASE_URL + '</td></tr>');
+	response.write('</tbody> </table> </main>');
+	response.write('<footer> <p>Team</p> <ul> <li>Christian Jakl</li> <li>Lutz Kiefer</li> <li>Astrid Krause</li> <li>Guenter Krause</li> 	<li>Simone Kuebler</li>	<li>Heribert Roth</li>   </ul>  </footer>');
+	response.write('</body></html>\n');
+     response.end();
 
-      res.write(data);
-      console.log('Step 4');
-
-      res.end();
   });
-}).listen(8080); 
+
+
+});
+
+
+// listen on localhost:8000
+server.listen(8000);
+console.log("Server listening at http://127.0.0.1:8000/");
