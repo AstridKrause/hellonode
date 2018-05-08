ac0bab88184287f31dc4b73132ce541bb7d294e5
Astrid Krause <astrid.krause@dxc.com>
2018-05-08 09:04:15 +0000

    Test

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ var fs = require('fs');
 http.createServer(function (req, res) {
   //Open a file on the server and return it's content:
   fs.readFile('demofile2.html', function(err, data) {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+       return res.end("404 Not Found");
+    }  
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
     return res.end();
